7bd8096eb496c55eebf413247862eecb00d5bc0d
Anelle Mensah <aam2ds@virginia.edu>
2023-11-08 18:37:59 -0500

    render Search, Nav, and Photo components and remove Vite starter code

--- react-gallery-app/src/App.jsx
+++ react-gallery-app/src/App.jsx
@@ -1,10 +1,9 @@
 // import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
 // import apiKey from './components/config.js'
 import Search from './components/Search.jsx'
 import Nav from './components/Nav.jsx'
+import Photo from './components/Photo.jsx'
 
 
 function App() {
@@ -14,29 +13,11 @@ function App() {
     <div className='container'>
       <Search />
       <Nav />
+      <div className='photo-container'>
+        <h2>Results</h2>
+        <Photo />
+      </div>
     </div>
-    // <>
-    //   <div>
-    //     <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-    //       <img src={viteLogo} className="logo" alt="Vite logo" />
-    //     </a>
-    //     <a href="https://react.dev" target="_blank" rel="noreferrer">
-    //       <img src={reactLogo} className="logo react" alt="React logo" />
-    //     </a>
-    //   </div>
-    //   <h1>Vite + React</h1>
-    //   <div className="card">
-    //     <button onClick={() => setCount((count) => count + 1)}>
-    //       count is {count}
-    //     </button>
-    //     <p>
-    //       Edit <code>src/App.jsx</code> and save to test HMR
-    //     </p>
-    //   </div>
-    //   <p className="read-the-docs">
-    //     Click on the Vite and React logos to learn more
-    //   </p>
-    // </>
   )
 }
 
