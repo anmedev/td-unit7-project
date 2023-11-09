a7b5dc773dce3f22eca2a2e6c285445043771406
Anelle Mensah <aam2ds@virginia.edu>
2023-11-09 06:31:07 -0500

    remove Search and Nav component tags from App component tags and Search and Nav imports

--- react-gallery-app/src/index.jsx
+++ react-gallery-app/src/index.jsx
@@ -3,16 +3,11 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
-import Search from "./components/Search.jsx";
-import Nav from "./components/Nav.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App 
-        search={<Search />}
-        nav={<Nav />}
-      />
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
