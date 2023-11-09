d73424111be433b37974c771cc9c34c23cbe9563
Anelle Mensah <aam2ds@virginia.edu>
2023-11-09 05:27:06 -0500

    comment extra photo

--- react-gallery-app/src/components/PhotoList.jsx
+++ react-gallery-app/src/components/PhotoList.jsx
@@ -10,9 +10,9 @@ const PhotoList = () => {
           <li>
             <img src="https://farm5.staticflickr.com/4343/37175099045_0d3a249629.jpg" alt="" />
           </li>
-          <li>
+          {/* <li>
             <img src="https://farm5.staticflickr.com/4425/36337012384_ba3365621e.jpg" alt="" />
-          </li>
+          </li> */}
 
           <li className="not-found">
             <h3>No Results Found</h3>
