841506c31764a79eb29b77cbfcee9f90483cfad7
Anelle Mensah <aam2ds@virginia.edu>
2023-11-09 05:23:19 -0500

    render Search and Nav components in App component tag

--- react-gallery-app/src/index.jsx
+++ react-gallery-app/src/index.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
+import Search from "./components/Search.jsx";
+import Nav from "./components/Nav.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <App 
+        search={<Search />}
+        nav={<Nav />}
+      />
+    </BrowserRouter>
+  </React.StrictMode>
+);
