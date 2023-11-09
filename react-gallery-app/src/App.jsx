d017e86705332eb0e7ece9be25a56c4cdcb37e96
Anelle Mensah <aam2ds@virginia.edu>
2023-11-09 05:25:32 -0500

    create routes and render PhotoList component

--- react-gallery-app/src/App.jsx
+++ react-gallery-app/src/App.jsx
@@ -4,6 +4,9 @@ import './App.css'
 import Search from './components/Search.jsx'
 import Nav from './components/Nav.jsx'
 import Photo from './components/Photo.jsx'
+import PhotoList from './components/PhotoList.jsx'
+import {Route, Routes} from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
 
 
 function App() {
@@ -11,6 +14,12 @@ function App() {
 
   return (
     <div className='container'>
+      <Routes>
+      <Route path='/' element={<Navigate to='/cats' />} />
+        <Route path='/cats' element={<PhotoList />}></Route>
+        <Route path='/dogs' element={<PhotoList />}></Route>
+        <Route path='/computers' element={<PhotoList />}></Route>
+      </Routes>
       <Search />
       <Nav />
       <div className='photo-container'>
